a6f99c9a9a0d449cd46963736e10a0e00b73c9fc
Soul-T-Nuts <78574800+Soul-T-Nuts@users.noreply.github.com>
2024-09-04 02:25:53 +0000

    Added vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '@/styles/globals.css'
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
 import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PlasmicRootProvider Head={Head}>
       <Component {...pageProps} />
+      <Analytics />
     </PlasmicRootProvider>
   );
 }
